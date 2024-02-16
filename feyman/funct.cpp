421dd0d6fd0dec524df41a555cac90e53ae8dbf9
blociss <blaucisse@gmail.com>
2024-02-16 13:23:06 +0100

    up

--- feyman/funct.cpp
+++ feyman/funct.cpp
@@ -84,6 +84,7 @@ std::vector<int> flip_signature(const FeynmanGraph& G, const std::vector<int>& p
 
     return b;
 }
+//define factorial of an integer.
 int factorial(int n) {
     if (n <= 1) {
         return 1;
@@ -91,7 +92,32 @@ int factorial(int n) {
         return n * factorial(n - 1);
     }
 }
-    std::vector<std::tuple<int, std::vector<int> > > signature_and_multiplicities(const FeynmanGraph& G, std::vector<int> a ) {
+
+//print vector function
+void printVector(const std::vector<int> &vec)
+{
+    for (int value : vec){
+        if (value==*vec.begin()){
+            std::cout<<"  "<< value;
+        }
+        else{
+            std::cout << ", "<<value;
+        }
+    }
+    std::cout << std::endl;
+}
+
+std::vector<std::vector<int>> permutation(std::vector<int>& vec) {
+    // Sort the vector to ensure permutations are unique
+    std::sort(vec.begin(), vec.end());
+
+    // Collect permutations and print them
+    do {
+        printVector(vec);
+    } while (std::next_permutation(vec.begin(), vec.end()));
+}
+
+std::vector<std::tuple<int, std::vector<int> > > signature_and_multiplicities(const FeynmanGraph& G, std::vector<int> a ) {
     std::vector<Edge> ee=G.edges();
     std::vector<int> p;
     std::vector<std::tuple<int, std::vector<int> > > b;
@@ -103,7 +129,58 @@ int factorial(int n) {
         return b;
     }
     else{
-        for ()
+        for (size_t i=0; i<=a.size();i++){
+           int ai=a[i];
+           Edge ei=ee[i];
+           if (ai==0 && ei.src!=ei.dst){
+            l[ei.src]==1;
+            l[ei.dst]==1;
+           }
+        }
+        for (size_t i=0;i<=l.size();i++){
+            int li=l[i];
+            if (li==1){
+                p.push_back(i);
+            }
+        }
+        std::vector<std::vector<int>> per=permutation(p);
+        int dd=factorial(G.nv())/a.size() ;
+        for (std::vector<int> pe:per){
+            y.push_back(flip_signature(G,pe,a));
+        }
+        std::unordered_map<std::vector<int>, int, VectorHash, VectorEqual> py=count_member(y);
+        for (auto& [key,val]:py){
+            b.push_back(std::make_tuple(dd*val,key));
+        }
+        if (b.size()==1){
+            return b;
+        }
+        else{
+            std::vector<std::tuple<int,std::vector<int> > > group;
+            for (const auto& pair1:b){
+                int n=std::get<0>(pair1);
+                std::vector<int> values1=std::get<1>(pair1);
+                int mm=2*n;
+                auto it1=std::find(group.begin(),group.end(),pair1);
+                auto it2=std::find(group.begin(),group.end(),std::make_tuple(mm,values1));
+                if (it1!=group.end()  || it2!=group.end()){
+                    continue;
+                }
+                    bool equiv = false;
+
+             for (const auto& pair2:b){
+                int m=std::get<0>(pair2);
+                std::vector<int> values2=std::get<1>(pair2);
+                int mm=2*n;
+                auto it1=std::find(group.begin(),group.end(),pair2);
+                auto it2=std::find(group.begin(),group.end(),std::make_tuple(2*mm,values1));
+                if (it1!=group.end()  || it2!=group.end()){
+                    continue;
+                }
+            }
+        }
+
+
     }
 }
 
@@ -160,6 +237,8 @@ int main() {
         std::cout << ", ";
     }
     std::cout<<"}" << std::endl;
+
+ permutation(o);
 
     return 0;
 }
