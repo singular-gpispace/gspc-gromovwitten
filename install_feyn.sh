566dec391deaf7f561aa1b645ef91e1602a4ac08
blociss <blaucisse@gmail.com>
2024-05-19 13:15:54 +0200

    uo

--- install_feyn.sh
+++ install_feyn.sh
@@ -20,9 +20,8 @@ cmake --build "~/gpi/try_gpi/gpispace/build" --target install -j $(nproc) > erro
 
 
 ###############################################
-cd ~/spack/opt/spack/linux-ubuntu22.04-skylake/gcc-11.3.0/gpi-space-23.06-gxye6b7ngsnbxnzjkfsfqtvanynyghdk/bin
+cd ~/spack/opt/spack/linux-ubuntu22.04-skylake/gcc-11.3.0/gpi-space-23.06-gxye6b7ngsnbxnzjkfsfqtvanynyghdk/bin && ./gspc-logging-to-stdout.exe --port 9876 > ~/gpi/try_gpi/gpispace/monitor.txt 2>&1
 
-./gspc-logging-to-stdout.exe --port 9876 > ~/gpi/try_gpi/gpispace/monitor.txt 2>&1
 
 
 
@@ -68,7 +67,7 @@ time ~/gpi/try_gpi/gpispace/bin/bin/feynman \
   --rif-strategy ssh \
   --topology "worker:7" \
   --N 9 \
-  --degree 4 \
+  --degree 8 \
   --graph "{{1,2},{1,2},{1,3},{2,4},{3,4},{3,5},{4,6},{5,6},{5,6}}" \
   --log-host localhost \
   --log-port 9876
