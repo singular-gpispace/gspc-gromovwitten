8f2f5112af06df784b6ac21f3c988328e7bd94f2
blociss <blaucisse@gmail.com>
2025-05-05 17:15:06 +0200

    new gpi-space

--- install.sh
+++ install.sh
@@ -1,24 +1,37 @@
-rm -r ~/gpi/try_gpi/gpispace/build/*
+rm -r ~/gpi/try_gpi/gspc-gromovwitten/build/* 
+rm -r ~/gpi/try_gpi/gspc-gromovwitten/bin/*
+
+# Compile and run generate_fey.cpp to generate new feynman.xpnet
+ cd "/home/atraore/gpi/try_gpi/gspc-gromovwitten/cpp/" && g++ generate_fey.cpp -o generate_fey && "/home/atraore/gpi/try_gpi/gspc-gromovwitten/cpp/"generate_fey
+mv ~/gpi/try_gpi/gspc-gromovwitten/cpp/feynman.xpnet ~/gpi/try_gpi/gspc-gromovwitten/workflow/feynman.xpnet
+
+# Generate SVG workflow diagram
+spack load gpi-space@24.12
+pnetc ~/gpi/try_gpi/gspc-gromovwitten/workflow/feynman.xpnet | pnet2dot | dot -T svg > ~/gpi/try_gpi/gspc-gromovwitten/workflow/feynman.svg
+
+# Set GPISpace root path
+GPISPACE_ROOT="/home/atraore/singular-gpispace/spack/opt/spack/linux-ubuntu22.04-skylake/gcc-11.3.0/gpi-space-24.12-jz6b4m6ql54fmhkpq6gbico2neic3kd5"
+export GSPC_HOME=$GPISPACE_ROOT
+
 cmake \
-  -D GPISpace_ROOT=~/spack/ \
- -D GSPC_WITH_MONITOR_APP=OFF \
-  -D GPISpace_ROOT=~/spack/opt/spack/linux-ubuntu22.04-skylake/gcc-11.3.0/gpi-space-23.06-gxye6b7ngsnbxnzjkfsfqtvanynyghdk \
-  -D Boost_ROOT=~/spack/opt/spack/linux-ubuntu22.04-skylake/gcc-11.3.0/boost-1.63.0-q5xh5vlq3ihagc7f5ql4erhsofvamt6m \
-  -D CMAKE_INSTALL_PREFIX=~/gpi/try_gpi/gpispace/bin \
+  -D GPISpace_ROOT=$GPISPACE_ROOT \
+  -D GSPC_WITH_MONITOR_APP=OFF \
+  -D Boost_ROOT=$GPISPACE_ROOT \
+  -D CMAKE_INSTALL_PREFIX=~/gpi/try_gpi/gspc-gromovwitten/bin \
   -D Boost_NO_BOOST_CMAKE=TRUE \
-  -B "~/gpi/try_gpi/gpispace/build" \
-  -S "~/gpi/try_gpi/gpispace/"
+  -B "~/gpi/try_gpi/gspc-gromovwitten/build" \
+  -S "~/gpi/try_gpi/gspc-gromovwitten/"
 
 
 cmake \
-  --build "~/gpi/try_gpi/gpispace/build" \
+  --build "~/gpi/try_gpi/gspc-gromovwitten/build" \
   --target install \
   -j $(nproc)
 
 
-time ~/gpi/try_gpi/gpispace/bin/bin/feynman \
-  --gspc-home ~/spack/opt/spack/linux-ubuntu22.04-skylake/gcc-11.3.0/gpi-space-23.06-gxye6b7ngsnbxnzjkfsfqtvanynyghdk/ \
-  --nodefile ~/gpispace/nodefile \
+time ~/gpi/try_gpi/gspc-gromovwitten/bin/bin/feynman \
+  --gspc-home ~/singular-gpispace/spack/opt/spack/linux-ubuntu22.04-skylake/gcc-11.3.0/gpi-space-24.12-jz6b4m6ql54fmhkpq6gbico2neic3kd5/ \
+  --nodefile ~/gpi/try_gpi/gspc-gromovwitten/nodefile \
   --rif-strategy ssh \
   --topology "worker:7" \
   --N 6 \
@@ -27,12 +40,10 @@ time ~/gpi/try_gpi/gpispace/bin/bin/feynman \
   --log-host localhost \
   --log-port 9876
 
-spack load gpi-space@23.06
 
-
-time ~/gpi/try_gpi/gpispace/bin/bin/feynman \
-  --gspc-home ~/spack/opt/spack/linux-ubuntu22.04-skylake/gcc-11.3.0/gpi-space-23.06-gxye6b7ngsnbxnzjkfsfqtvanynyghdk/ \
-  --nodefile ~/gpispace/nodefile \
+time ~/gpi/try_gpi/gspc-gromovwitten/bin/bin/feynman \
+  --gspc-home ~/singular-gpispace/spack/opt/spack/linux-ubuntu22.04-skylake/gcc-11.3.0/gpi-space-24.12-jz6b4m6ql54fmhkpq6gbico2neic3kd5/ \
+  --nodefile ~/gpi/try_gpi/gspc-gromovwitten/nodefile \
   --rif-strategy ssh \
   --topology "worker:7" \
   --N 6 \
@@ -42,18 +53,13 @@ time ~/gpi/try_gpi/gpispace/bin/bin/feynman \
   --log-port 9876
 
 
-time ~/gpi/try_gpi/gpispace/bin/bin/feynman \
-  --gspc-home ~/spack/opt/spack/linux-ubuntu22.04-skylake/gcc-11.3.0/gpi-space-23.06-gxye6b7ngsnbxnzjkfsfqtvanynyghdk/ \
-  --nodefile ~/gpispace/nodefile \
-  --rif-strategy ssh \
-  --topology "worker:7" \
-  --N 9 \
-  --degree 4 \
-  --graph "{ {1, 2}, {1, 2}, {1, 3}, {2, 4}, {3, 4}, {3, 5}, {4, 6}, {5, 6}, {5, 6} }" \
-  --log-host localhost \
-  --log-port 9876
-
-
-cd ~/spack/opt/spack/linux-ubuntu22.04-skylake/gcc-11.3.0/gpi-space-23.06-gxye6b7ngsnbxnzjkfsfqtvanynyghdk/bin && ./gspc-logging-to-stdout.exe --port 9876 > ~/gpi/try_gpi/gpispace/monitor.txt 2>&1
-
-: > monitor.txt # erase the content of the file monitor.txt
+# time ~/gpi/try_gpi/gspc-gromovwitten/bin/bin/feynman \
+#   --gspc-home ~/singular-gpispace/spack/opt/spack/linux-ubuntu22.04-skylake/gcc-11.3.0/gpi-space-24.12-jz6b4m6ql54fmhkpq6gbico2neic3kd5/ \
+#   --nodefile ~/gpi/try_gpi/gspc-gromovwitten/nodefile \
+#   --rif-strategy ssh \
+#   --topology "worker:7" \
+#   --N 9 \
+#   --degree 4 \
+#   --graph "{ {1, 2}, {1, 2}, {1, 3}, {2, 4}, {3, 4}, {3, 5}, {4, 6}, {5, 6}, {5, 6} }" \
+#   --log-host localhost \
+#   --log-port 9876
