a24e99b8a14cca54db736a6101ca6c36f9dab685
blociss <blaucisse@gmail.com>
2025-05-07 10:56:14 +0200

    up

--- install.sh
+++ install.sh
@@ -1,24 +1,32 @@
-rm -r ~/gpi/try_gpi/gpispace/build/*
+rm -r ~/gpi/try_gpi/gspc-gromovwitten/build/* 
+rm -r ~/gpi/try_gpi/gspc-gromovwitten/bin/*
+# Generate SVG workflow diagram
+spack load gpi-space@24.12
+pnetc ~/gpi/try_gpi/gspc-gromovwitten/workflow/feynman.xpnet | pnet2dot | dot -T svg > ~/gpi/try_gpi/gspc-gromovwitten/workflow/feynman.svg
+
+# Set GPISpace root path
+GPISPACE_ROOT="/home/atraore/singular-gpispace/spack/opt/spack/linux-ubuntu22.04-skylake/gcc-11.3.0/gpi-space-24.12-jz6b4m6ql54fmhkpq6gbico2neic3kd5"
+export GSPC_HOME=$GPISPACE_ROOT
+
 cmake \
-  -D GPISpace_ROOT=~/spack/ \
- -D GSPC_WITH_MONITOR_APP=OFF \
-  -D GPISpace_ROOT=~/spack/opt/spack/linux-ubuntu22.04-skylake/gcc-11.3.0/gpi-space-23.06-gxye6b7ngsnbxnzjkfsfqtvanynyghdk \
-  -D Boost_ROOT=~/spack/opt/spack/linux-ubuntu22.04-skylake/gcc-11.3.0/boost-1.63.0-q5xh5vlq3ihagc7f5ql4erhsofvamt6m \
-  -D CMAKE_INSTALL_PREFIX=~/gpi/try_gpi/gpispace/bin \
+  -D GPISpace_ROOT=$GPISPACE_ROOT \
+  -D GSPC_WITH_MONITOR_APP=OFF \
+  -D Boost_ROOT=$GPISPACE_ROOT \
+  -D CMAKE_INSTALL_PREFIX=~/gpi/try_gpi/gspc-gromovwitten/bin \
   -D Boost_NO_BOOST_CMAKE=TRUE \
-  -B "~/gpi/try_gpi/gpispace/build" \
-  -S "~/gpi/try_gpi/gpispace/"
+  -B "~/gpi/try_gpi/gspc-gromovwitten/build" \
+  -S "~/gpi/try_gpi/gspc-gromovwitten/"
 
 
 cmake \
-  --build "~/gpi/try_gpi/gpispace/build" \
+  --build "~/gpi/try_gpi/gspc-gromovwitten/build" \
   --target install \
   -j $(nproc)
 
 
-time ~/gpi/try_gpi/gpispace/bin/bin/feynman \
-  --gspc-home ~/spack/opt/spack/linux-ubuntu22.04-skylake/gcc-11.3.0/gpi-space-23.06-gxye6b7ngsnbxnzjkfsfqtvanynyghdk/ \
-  --nodefile ~/gpispace/nodefile \
+time ~/gpi/try_gpi/gspc-gromovwitten/bin/bin/feynman \
+  --gspc-home $GPISPACE_ROOT/ \
+  --nodefile ~/gpi/try_gpi/gspc-gromovwitten/nodefile \
   --rif-strategy ssh \
   --topology "worker:7" \
   --N 6 \
@@ -27,4 +35,4 @@ time ~/gpi/try_gpi/gpispace/bin/bin/feynman \
   --log-host localhost \
   --log-port 9876
 
-spack load gpi-space@23.06
+spack load gpi-space@24.12
